4a42981bb783394239a17e0da2faf22efbca7de6
rkandras <andristarr@gmail.com>
2020-07-07 11:19:29 +0200

    Fixing comment, code cleanup, found pointer to player object.

---
+++ game_func_hook/src/game_declars.rs
@@ -0,0 +1,4 @@
+pub enum GameDeclarations {
+    ActivePlayerInClientWorld = 0x2C,
+    PlayerNameInActivePlayer = 0x78
+}
--- game_func_hook/src/lib.rs
+++ game_func_hook/src/lib.rs
@@ -1,10 +1,12 @@
+mod game_declars;
+
 extern crate winapi;
 extern crate user32;
 extern crate kernel32;
 extern crate libc;
 extern crate detour;
 
-use std::ffi::{OsStr};
+use std::ffi::{OsStr, CString};
 use std::iter::once;
 use std::os::windows::ffi::OsStrExt;
 use kernel32::{GetModuleHandleW};
@@ -12,28 +14,34 @@ use winapi::{DWORD, MB_OK, c_void};
 use user32::MessageBoxW;
 use std::ptr::null_mut;
 use detour::static_detour;
+use crate::game_declars::GameDeclarations;
 
 fn win32_string(value : &str ) -> Vec<u16> {
     OsStr::new( value ).encode_wide().chain( once( 0 ) ).collect()
 }
 
+// since this is thiscall, the easiest way to detour it is to use fastcall and make the second
+// parameter a dead param to not touch edx at all (this pointer comes in ecx)
 static_detour! {
     static CwTickFn: unsafe extern "fastcall" fn(DWORD, DWORD, f32) -> c_void;
 }
 
 enum GameOffset {
     ClientWorldTick = 0xCAE0,
+    PlayerTick = 0x50730
 }
 
-extern "fastcall" fn hooked_cw_tick(p_this: DWORD, _edx: DWORD, float32: f32) -> c_void {
+extern "fastcall" fn hooked_go_tick(p_this: DWORD, _edx: DWORD, float32: f32) -> c_void {
     unsafe {
-        MessageBoxW(null_mut(), win32_string("Hello from hooked func").as_ptr(), win32_string("Greetings").as_ptr(), MB_OK);
+        let cw_this_str = format!("value of p_this: {:X?}", p_this);
+
+        MessageBoxW(null_mut(), win32_string(&cw_this_str).as_ptr(), win32_string("Real_p_this").as_ptr(), MB_OK);
 
         return CwTickFn.call(p_this, _edx, float32);
     }
 }
 
-type CwTickFnType = unsafe extern "fastcall" fn(DWORD, DWORD, f32) -> c_void;
+type GameObjTick = unsafe extern "fastcall" fn(DWORD, DWORD, f32) -> c_void;
 
 // entry point
 #[no_mangle]
@@ -60,20 +68,20 @@ fn hook_cwtick() {
     unsafe {
         let module_name = "GameLogic.dll";
 
-        let func_loc: CwTickFnType = hook_game_func(module_name); // "ClientWorld::Tick(float)"
+        let func_loc: GameObjTick = hook_game_func(module_name); // "ClientWorld::Tick(float)"
 
-        let hook_closure = | pthis: DWORD, _edx: DWORD, float32: f32 | -> c_void { hooked_cw_tick(pthis, _edx, float32) };
+        let hook_closure = | pthis: DWORD, _edx: DWORD, float32: f32 | -> c_void { hooked_go_tick(pthis, _edx, float32) };
 
         CwTickFn.initialize(func_loc, hook_closure).unwrap().enable();
     }
 }
 
-unsafe fn hook_game_func(module_name: &str) -> CwTickFnType {
+unsafe fn hook_game_func(module_name: &str) -> GameObjTick {
     let module_handle = GetModuleHandleW(win32_string(module_name).as_ptr()) as DWORD;
 
-    let func_addr = module_handle + GameOffset::ClientWorldTick as u32;
+    let func_addr = module_handle + GameOffset::PlayerTick as u32;
 
-    let func_address: CwTickFnType = std::mem::transmute::<DWORD, CwTickFnType>(func_addr);
+    let func_address: GameObjTick = std::mem::transmute::<DWORD, GameObjTick>(func_addr);
 
     func_address
 }
