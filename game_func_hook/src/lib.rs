cfa96c2a9caf9fa6447a430a2d94ad6d1829de1e
rkandras <andristarr@gmail.com>
2020-07-13 22:47:37 +0200

    Adding game class + modification of jump and walkspeed.

--- game_func_hook/src/lib.rs
+++ game_func_hook/src/lib.rs
@@ -6,7 +6,7 @@ extern crate kernel32;
 extern crate libc;
 extern crate detour;
 
-use std::ffi::{OsStr, CString};
+use std::ffi::{OsStr, CString, CStr};
 use std::iter::once;
 use std::os::windows::ffi::OsStrExt;
 use kernel32::{GetModuleHandleW};
@@ -31,11 +31,45 @@ enum GameOffset {
     PlayerTick = 0x50730
 }
 
+static mut haveBeenCalled: bool = false;
+
 extern "fastcall" fn hooked_go_tick(p_this: DWORD, _edx: DWORD, float32: f32) -> c_void {
     unsafe {
-        let cw_this_str = format!("value of p_this: {:X?}", p_this);
 
-        MessageBoxW(null_mut(), win32_string(&cw_this_str).as_ptr(), win32_string("Real_p_this").as_ptr(), MB_OK);
+        if haveBeenCalled == false {
+
+            let p_copy = p_this;
+
+            haveBeenCalled = true;
+
+            let player_name_loc = p_copy + GameDeclarations::PlayerNameInActivePlayer as u32;
+
+            let player_name_as_mut = std::mem::transmute_copy::<DWORD, *mut i8>(&player_name_loc);
+
+            let player_name = CStr::from_ptr(player_name_as_mut);
+
+            let player_mana_loc = std::mem::transmute::<DWORD, *const u32>(p_copy + GameDeclarations::ManaInPlayer as u32);
+
+            let player_mana = std::ptr::read(player_mana_loc);
+
+            let player_walk_speed_location = std::mem::transmute::<DWORD, *mut f32>(p_copy + GameDeclarations::WalkSpeedInPlayer as u32);
+
+            let player_jump_speed_location = std::mem::transmute::<DWORD, *mut f32>(p_copy + GameDeclarations::JumpSpeedInPlayer as u32);
+
+            let desired_player_walk_speed = 5000.0 as f32;
+
+            let desired_jump_speed = 10000.0 as f32;
+
+            std::ptr::write(player_walk_speed_location, desired_player_walk_speed);
+
+            std::ptr::write(player_jump_speed_location, desired_jump_speed);
+
+            let player_walk_speed = std::ptr::read(player_walk_speed_location);
+
+            let cw_this_str = format!("value of p_this: {:X?}, player name is: {:?}, players current mana is: {:?}, player walk speed is: {:?}", p_copy, player_name, player_mana, player_walk_speed);
+
+            MessageBoxW(null_mut(), win32_string(&cw_this_str).as_ptr(), win32_string("p_this").as_ptr(), MB_OK);
+        }
 
         return CwTickFn.call(p_this, _edx, float32);
     }
@@ -68,7 +102,7 @@ fn hook_cwtick() {
     unsafe {
         let module_name = "GameLogic.dll";
 
-        let func_loc: GameObjTick = hook_game_func(module_name); // "ClientWorld::Tick(float)"
+        let func_loc: GameObjTick = hook_game_func(module_name); // "Player::Tick(float)"
 
         let hook_closure = | pthis: DWORD, _edx: DWORD, float32: f32 | -> c_void { hooked_go_tick(pthis, _edx, float32) };
 
